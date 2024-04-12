04009b7c6e44ee2b589a8045a245d65a96c2e5ac
NomisIV <simon@nomisiv.com>
2024-04-12 19:26:12 +0200

    nix stuff

--- flake.nix
+++ flake.nix
@@ -5,18 +5,21 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: let
-    pkgs = import nixpkgs {system="x86_64-linux";};
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = import nixpkgs {system = "x86_64-linux";};
   in {
-
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
     devShells.x86_64-linux.default = pkgs.mkShell {
       packages = with pkgs; [
-       nodePackages_latest.typescript-language-server
-      vscode-langservers-extracted
+        nodePackages_latest.typescript-language-server
+        vscode-langservers-extracted
+        python3
       ];
     };
     formatter.x86_64-linux = pkgs.alejandra;
