15f102c3972886ef9e8f62c900aa9d1960a8a3ef
NomisIV <simon@nomisiv.com>
2024-04-12 19:04:49 +0200

    Initialize nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    pkgs = import nixpkgs {system="x86_64-linux";};
+  in {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+       nodePackages_latest.typescript-language-server
+      vscode-langservers-extracted
+      ];
+    };
+    formatter.x86_64-linux = pkgs.alejandra;
+  };
+}
