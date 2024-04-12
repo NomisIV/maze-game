b382e35f2bb533df1bdb9ec36ce300f158b7581a
Herman Lauenstein <herman.lauenstein@gmail.com>
2024-04-12 20:15:53 +0200

    Maze generator

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { Maze } from "./maze.js";
 
+let maze = new Maze(20, 15);
+
 window.setup = () => {
     window.createCanvas(window.innerWidth, window.innerHeight);
 
@@ -12,8 +14,14 @@ window.windowResized = () => {
 window.draw = () => {
     window.background(100, 100, 100);
 
-    window.fill(255, 100, 10);
-    window.rect(50, 50, 200, 150);
+    
+    window.stroke(255, 255, 255);
+    window.strokeWeight(3);
+
+    for (let [x1, y1, x2, y2] of maze.getWalls()) {
+        window.line(x1 * 20 + 100, y1 * 20 + 100, x2 * 20 + 100, y2 * 20 + 100);
+    }
+
 };
 
 window.keyPressed = () => {
--- src/maze.js
+++ src/maze.js
@@ -1,26 +1,69 @@
 export class Maze {
     constructor(width, height) {
-
-        this.walls = new Set();
-
-        this.addWall(1, 2, 1, 3);
-        this.addWall(1, 3, 2, 3);
-        this.addWall(2, 3, 3, 3);
+        this._walls = new Set();
+        this._construct(0, 0, width, height, true);
     }
 
-    wallString(x1, y1, x2, y2) {
+    _wallString(x1, y1, x2, y2) {
         return [`${x1} ${y1}`, `${x2} ${y2}`].sort().join(" ");
     }
 
     addWall(x1, y1, x2, y2) {
-        this.walls.add(this.wallString(x1, y1, x2, y2));
+        this._walls.add(this._wallString(x1, y1, x2, y2));
     }
 
     hasWall(x1, y1, x2, y2) {
-        return this.walls.has(this.wallString(x1, y1, x2, y2));
+        return this._walls.has(this._wallString(x1, y1, x2, y2));
     }
 
-    _construct() {
+    getWalls() {
+        let walls = [];
+        for (let wall of this._walls) {
+            walls.push(wall.split(" ").map(n => parseInt(n)));
+        }
+        return walls;
+    }
 
+    _construct(minX, minY, maxX, maxY, outerWalls = false) {
+        if (outerWalls) {
+            for (let x = minX; x < maxX; x++) {
+                this.addWall(x, minY, x + 1, minY);
+                this.addWall(x, maxY, x + 1, maxY);
+            }
+            for (let y = minY; y < maxY; y++) {
+                this.addWall(minX, y, minX, y + 1);
+                this.addWall(maxX, y, maxX, y + 1);
+            }
+        }
+
+        let width = maxX - minX;
+        let height = maxY - minY;
+        if (width <= 1 || height <= 1) return;
+
+        if (Math.random() * (width + height) < width) {
+            // Split with a vertical wall
+
+            let splitX = minX + 1 + Math.floor(Math.random() * (width - 1));
+            let gapY = minY + Math.floor(Math.random() * height);
+
+            for (let y = minY; y < maxY; y++) {
+                if (y != gapY) this.addWall(splitX, y, splitX, y + 1);
+            }
+
+            this._construct(minX, minY, splitX, maxY);
+            this._construct(splitX, minY, maxX, maxY);
+        } else {
+            // Split with a horizontal wall
+
+            let splitY = minY + 1 + Math.floor(Math.random() * (height - 1));
+            let gapX = minX + Math.floor(Math.random() * width);
+
+            for (let x = minX; x < maxX; x++) {
+                if (x != gapX) this.addWall(x, splitY, x + 1, splitY);
+            }
+
+            this._construct(minX, minY, maxX, splitY);
+            this._construct(minX, splitY, maxX, maxY);
+        }
     }
 }
