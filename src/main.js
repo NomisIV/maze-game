72057fbda4779046deba08769d5566e0fb4c8f68
Herman Lauenstein <herman.lauenstein@gmail.com>
2024-04-14 15:17:43 +0200

    Put the game state in a class

--- src/main.js
+++ src/main.js
@@ -4,90 +4,205 @@ import { cellMaze, layerMaze } from "./maze.js";
 import { Monster } from "./monster.js";
 import { Player } from "./player.js";
 
-let maze;
-let graphics;
-let player;
-let ammunitions = [];
-let monsters = [];
+class GameState {
+    constructor(levelIdx) {
+        this.graphics = null;
+        this.keyStack = [];
 
-let moveCooldown;
-let keyStack = [];
+        switch (levelIdx % 3) {
+            case 0:
+                this.maze = layerMaze(17, 4, 6);
+                this.levelExtra = [[Math.floor(Math.random() * 17), 0]];
+                break;
+
+            case 1:
+                this.maze = cellMaze(3, 3, 8, 8);
+
+                let pos1 = Math.floor(Math.random() * 24);
+                let pos2 = Math.floor(Math.random() * 24);
+                this.levelExtra = Math.random() < 0.5 ? [[pos1, 0], [pos2, 23]] : [[0, pos1], [23, pos2]];
+
+                break;
+            case 2:
+                this.maze = cellMaze(6, 6, 6, 6);
+    
+                this.levelExtra = [];
+    
+                for (let i = 0; i < 10; i++) {
+                    let x = 18;
+                    let y = 18;
+                    while (Math.hypot(x - 18, y - 18) < 8) {
+                        x = Math.floor(Math.random() * this.maze.width);
+                        y = Math.floor(Math.random() * this.maze.height);
+                    }
+                    this.levelExtra.push([x, y]);
+                }
+                break;
+        }
+
+        this.resetLevel();
+    }
+
+    resetLevel() {
+        this.moveCooldown = 0;
+
+        let oldGraphics = this.graphics;
+
+        switch (levelIdx % 3) {
+            case 0:
+                this.graphics = new Graphics("sand");
+                this.player = new Player(8, 23);
+                this.ammunitions = this.levelExtra.slice();
+                this.monsters = [new Monster(8, 30, "minotaur", 20)];
+                break;
+            case 1:
+                this.graphics = new Graphics("mansion");
+                this.player = new Player(8, 19);
+                this.ammunitions = this.levelExtra.slice();
+                this.monsters = this.levelExtra.map(([x, y]) => new Monster(x, y, "bat", 32));
+                break;
+            case 2:
+                this.graphics = new Graphics("hell");
+                this.player = new Player(18, 18);
+                this.player.ammunition = Infinity;
+                this.ammunitions = [];
+                this.monsters = this.levelExtra.map(([x, y]) => new Monster(x, y, "dog", 24));
+                break;
+        }
+
+        if (oldGraphics !== null) {
+            this.graphics.copyExplored(oldGraphics);
+        }
+    }
+
+    draw() {
+        // Game logic
+        for (let i = 0; i < this.monsters.length; i++) {
+            let monster = this.monsters[i];
+            if (frameCount % monster.speed === 0) {
+                if (!monster.isDead) {
+                    let isOccupied = (x, y) => this.monsters.some((m, j) => j !== i && !m.isDead && m.posX === x && m.posY === y);
+                    monster.stepTowardsPlayer(this.player, this.maze, isOccupied);
+                }
+            }
+        }
+
+        if (this.moveCooldown === 0 && !this.player.isDead) {
+            for (let i = this.keyStack.length - 1; i >= 0; i--) {
+                let dir = null;
+
+                switch (this.keyStack[i]) {
+                    case 37: // LeftArrow
+                        dir = [-1, 0];
+                        break;
+                    case 38: // UpArrow
+                        dir = [0, -1];
+                        break;
+                    case 39: // RightArrow
+                        dir = [1, 0];
+                        break;
+                    case 40: // DownArrow
+                        dir = [0, 1];
+                        break;
+                }
+                if (dir !== null) {
+                    if (this.player.move(this.maze, dir[0], dir[1])) {
+                        this.moveCooldown = PLAYER_SPEED;
+
+                        let key = this.keyStack.splice(i, 1)[0];
+                        this.keyStack.splice(0, 0, key);
+
+                        break;
+                    }
+                }
+            }
+        }
+        if (this.moveCooldown > 0) {
+            this.moveCooldown -= 1;
+        }
+
+        for (let i = this.ammunitions.length - 1; i >= 0; i--) {
+            let [x, y] = this.ammunitions[i];
+            if (this.player.posX === x && this.player.posY === y) {
+                this.player.ammunition += 1;
+                this.ammunitions.splice(i, 1);
+            }
+        }
+
+        for (let monster of this.monsters) {
+            if (this.player.posX === monster.posX && this.player.posY === monster.posY && !monster.isDead) {
+                this.player.isDead = true;
+            }
+        }
+
+        this.graphics.moveCamera();
+
+        // Drawing
+        this.graphics.startDrawing();
+        this.graphics.drawMaze(this.maze);
+
+        this.graphics.drawMonsters(this.monsters);
+        this.graphics.drawPlayer(this.player);
+
+        for (let [x, y] of this.ammunitions) {
+            this.graphics.drawAmmunition(x, y);
+        }
+
+        this.graphics.drawFogOfWar(this.maze, this.player);
+
+        this.graphics.endDrawing();
+
+        this.graphics.drawUI(this.player, this.monsters);
+    }
+
+    keyPressed(keyCode) {
+        switch (keyCode) {
+            case 13:
+                if (this.monsters.every((monster) => monster.isDead))
+                    resetLevel(true);
+                break;
+            case 32:
+                this.player.fireGun(this.monsters, this.maze);
+                break;
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+                if (this.keyStack.indexOf(keyCode) < 0) {
+                    this.keyStack.push(keyCode);
+                }
+                break;
+            case 82:
+                this.resetLevel();
+                break;
+        }
+    }
+
+    keyReleased(keyCode) {
+        switch (window.keyCode) {
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+                let idx = this.keyStack.indexOf(keyCode);
+                if (idx >= 0) {
+                    this.keyStack.splice(idx, 1);
+                }
+                break;
+        }
+    }
+}
+
+let gameState;
+let levelIdx = 0;
 
 window.preload = () => {
     window.angleMode(window.DEGREES);
     loadGraphics();
 }
 
-let levelExtra;
-function loadLevel(levelIdx, resetMaze) {
-    moveCooldown = 0;
-
-    let oldGraphics = graphics;
-
-    switch (levelIdx % 3) {
-        case 0:
-            if (resetMaze) {
-                maze = layerMaze(17, 4, 6);
-                levelExtra = [[Math.floor(Math.random() * 17), 0]];
-            }
-            graphics = new Graphics("sand");
-            player = new Player(8, 23);
-
-            ammunitions = levelExtra.slice();
-            monsters = [new Monster(8, 30, "minotaur", 20)];
-            break;
-        case 1:
-            if (resetMaze) {
-                maze = cellMaze(3, 3, 8, 8);
-
-                let pos1 = Math.floor(Math.random() * 24);
-                let pos2 = Math.floor(Math.random() * 24);
-                levelExtra = Math.random() < 0.5 ? [[pos1, 0], [pos2, 23]] : [[0, pos1], [23, pos2]];
-            }
-            graphics = new Graphics("mansion");
-            player = new Player(8, 19);
-
-            ammunitions = levelExtra.slice();
-            monsters = levelExtra.map(([x, y]) => new Monster(x, y, "bat", 32));
-            break;
-        case 2:
-            if (resetMaze) {
-                maze = cellMaze(6, 6, 6, 6);
-
-                levelExtra = [];
-
-                for (let i = 0; i < 10; i++) {
-                    let x = 18;
-                    let y = 18;
-                    while (Math.hypot(x - 18, y - 18) < 8) {
-                        x = Math.floor(Math.random() * maze.width);
-                        y = Math.floor(Math.random() * maze.height);
-                    }
-                    levelExtra.push([x, y]);
-                }
-            }
-            graphics = new Graphics("hell");
-            player = new Player(18, 18);
-            player.ammunition = Infinity;
-            ammunitions = [];
-
-            monsters = levelExtra.map(([x, y]) => new Monster(x, y, "dog", 24));
-            break;
-    }
-
-    if (!resetMaze) {
-        graphics.copyExplored(oldGraphics);
-    }
-}
-
-let currentLevel = 2;
-function resetGame(resetMaze) {
-    if (resetMaze) currentLevel += 1;
-    loadLevel(currentLevel, resetMaze);
-}
-
 window.setup = () => {
-    loadLevel(currentLevel, true)
+    gameState = new GameState(0);
 
     window.createCanvas(window.innerWidth, window.innerHeight);
 
@@ -98,119 +213,22 @@ window.windowResized = () => {
 }
 
 window.draw = () => {
-    // Game logic
-    for (let i = 0; i < monsters.length; i++) {
-        let monster = monsters[i];
-        if (frameCount % monster.speed === 0) {
-            if (!monster.isDead) {
-                let isOccupied = (x, y) => monsters.some((m, j) => j !== i && !m.isDead && m.posX === x && m.posY === y);
-                monster.stepTowardsPlayer(player, maze, isOccupied);
-            }
-        }
-    }
-
-    if (moveCooldown === 0 && keyStack.length > 0 && !player.isDead) {
-        for (let i = keyStack.length - 1; i >= 0; i--) {
-            let dir = null;
-
-            switch (keyStack[i]) {
-                case 37: // LeftArrow
-                    dir = [-1, 0];
-                    break;
-                case 38: // UpArrow
-                    dir = [0, -1];
-                    break;
-                case 39: // RightArrow
-                    dir = [1, 0];
-                    break;
-                case 40: // DownArrow
-                    dir = [0, 1];
-                    break;
-            }
-            if (dir !== null) {
-                if (player.move(maze, dir[0], dir[1])) {
-                    moveCooldown = PLAYER_SPEED;
-
-                    let key = keyStack.splice(i, 1)[0];
-                    keyStack.splice(0, 0, key);
-
-                    break;
-                }
-            }
-        }
-    }
-    if (moveCooldown > 0) {
-        moveCooldown -= 1;
-    }
-
-    for (let i = ammunitions.length - 1; i >= 0; i--) {
-        let [x, y] = ammunitions[i];
-        if (player.posX === x && player.posY === y) {
-            player.ammunition += 1;
-            ammunitions.splice(i, 1);
-        }
-    }
-
-    for (let monster of monsters) {
-        if (player.posX === monster.posX && player.posY === monster.posY && !monster.isDead) {
-            player.isDead = true;
-        }
-    }
-
-    graphics.moveCamera();
-
-    // Drawing
-    graphics.startDrawing();
-    graphics.drawMaze(maze);
-
-    graphics.drawMonsters(monsters);
-    graphics.drawPlayer(player);
-
-    for (let [x, y] of ammunitions) {
-        graphics.drawAmmunition(x, y);
-    }
-
-    graphics.drawFogOfWar(maze, player);
-
-    graphics.endDrawing();
-
-    graphics.drawUI(player, monsters);
+    gameState.draw();
 };
 
 window.keyPressed = () => {
     console.log("Pressed: :", window.keyCode);
     switch (window.keyCode) {
         case 13:
-            if (monsters.every((monster) => monster.isDead))
-                resetGame(true);
+            if (gameState.monsters.every((monster) => monster.isDead))
+                gameState = new GameState(++levelIdx);
             break;
-        case 32:
-            player.fireGun(monsters, maze);
-            break;
-        case 37:
-        case 38:
-        case 39:
-        case 40:
-            if (keyStack.indexOf(window.keyCode) < 0) {
-                keyStack.push(window.keyCode);
-            }
-            break;
-        case 82:
-            resetGame(false);
+        default:
+            gameState.keyPressed(window.keyCode);
             break;
     }
 };
 window.keyReleased = () => {
     console.log("Released: :", window.keyCode);
-    switch (window.keyCode) {
-        case 37:
-        case 38:
-        case 39:
-        case 40:
-            let idx = keyStack.indexOf(window.keyCode);
-            if (idx >= 0) {
-                keyStack.splice(idx, 1);
-            }
-            break;
-    }
+    gameState.keyReleased(window.keyCode);
 };
